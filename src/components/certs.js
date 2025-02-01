9c18c55fef0eea6f28d8eb02f6abb295294580cd
Moonshallow5 <ssandeepssingh55@gmail.com>
2025-02-01 11:47:49 +0800

    changed image import for vercel

--- src/components/certs.js
+++ src/components/certs.js
@@ -1,4 +1,5 @@
 
+import img1 from '@/assets/ML-Regress-and-classification.png';
 
 export const images=[
 
@@ -6,31 +7,31 @@ export const images=[
     {
         id:1,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:2,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:3,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:4,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:5,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
 
 
@@ -38,32 +39,32 @@ export const images=[
 
         id:6,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
 
     {
 
         id:7,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:8,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:9,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     {
 
         id:10,
         title:'ML-Regress-and-classification.png',
-        imgUrl:'/src/assets/ML-Regress-and-classification.png',
+        imgUrl:img1,
     },
     
 ]
